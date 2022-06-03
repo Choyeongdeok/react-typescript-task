feceae3f72ad2ee01bfdc479fcbeee11ee85ab3d
Cho Yeong Deok <angel987432@naver.com>
2022-06-03 13:34:51 +0900

    add edit, delete, done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import React, { useState } from 'react'
 import './App.css'
 import InputField from './component/InputField';
+import { Todo } from './model';
+import TodoList from './component/TodoList';
 
 const App: React.FC = () => {
 	const [todo, setTodo] = useState<string>('')
+	const [todos, setTodos] = useState<Todo[]>([])
 
-	console.log(todo)
+	const handleAdd = (e: React.FormEvent) => {
+		e.preventDefault()
+
+		if(todo) {
+			setTodos([...todos, {id: Date.now(), todo, isDone: false}])
+			setTodo('')
+		}
+	}
 
 	return (
 		<div className='App'>
 			<span className="heading">Task</span>
-			<InputField todo={todo} setTodo={setTodo}/>
+			<InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+			{/* {todos.map((t) => (
+				<li>{t.todo}</li>
+			))} */}
+			<TodoList todos={todos} setTodos={setTodos}/>
 		</div>
 	)
 }
