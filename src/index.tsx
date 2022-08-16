596cb39f5f32bb396229193a5ef732fc0ad98301
Cho Yeong Deok <angel987432@naver.com>
2022-08-16 12:54:35 +0900

    electron build migrate

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+/*
+    npm run electron:build
+    dist 하위에 빌드 파일 생성
+*/
+
 // const rootNode = document.getElementById('root')! // non-null assertion
 
 // ReactDOM.createRoot(rootNode).render( // React 18 부터 ReactDOM.render => ReactDOM.createRoot(node).render
